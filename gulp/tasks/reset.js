2b9f21ded63a4acd2f7aa4eb705a2341068e1b0f
ALEXX4891 <alexx4891@mail.ru>
2024-04-16 19:26:55 +0500

    удалит игнорируемые  файлы

--- gulp/tasks/reset.js
+++
@@ -1,9 +0,0 @@
-// import del from "del"; // модуль для удаления файлов и папок //npm i del -D
-// export const reset = () => {
-//   return del(app.path.clean);
-// }
-
-import { deleteAsync } from "del"; // модуль для удаления файлов и папок //npm i del -D
-export const reset = () => {
-    return deleteAsync(app.path.clean);
-};
--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = () => {
-  app.plugins.browserSync.init({
-    server: {
-      baseDir: `${app.path.build.html}` // базовая директория для сервера
-    },
-    notify: false, // выключаем уведомления
-    port: 3000 // порт
-  })
-}
