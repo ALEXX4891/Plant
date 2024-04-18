a0c70700baaf99e46a7121639d5dbb31868c39a0
ALEXX4891 <alexx4891@mail.ru>
2024-04-18 19:28:00 +0500

    частично выполнит  верстку детальной страницы заказа

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-import * as flsFunctions from "./modules/functions.js";
+// import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.isWebp();
+// flsFunctions.isWebp();
 
 //пример импорта библиотеки swiper из node_modules: 
 // import Swiper, { Navigation, Pagination } from "swiper";
@@ -17,8 +17,124 @@ flsFunctions.isWebp();
 //   },
 // })
 
-new SimpleBar(document.querySelector(".table-body-wrapper"), {
+const index = new SimpleBar(document.querySelector(".my-simplebar"), {
   scrollbarMaxSize: 85,
   autoHide: false,
   forceVisible: true,
+});
+
+ //горизонтальная прокрутка simplebar:
+let horizontalElements = document.querySelectorAll('[data-simplebar]');
+for (let element of horizontalElements)
+  element.onwheel = (event) => {
+    event.preventDefault();
+    let elementToScroll = element.querySelector('.simplebar-content-wrapper');
+
+    clearTimeout(elementToScroll.timer);
+    elementToScroll.timer = setTimeout(() => {
+      elementToScroll.scrollTo({ left: event.deltaY > 0 ? elementToScroll.scrollLeft + 500 : elementToScroll.scrollLeft - 500, behavior: 'smooth' });
+    }, 20);
+  };
+
+
+// popup:
+const popupLinks = document.querySelectorAll(".popup-link");
+const body = document.querySelector("body");
+const lockPadding = document.querySelectorAll(".lock-padding");
+// const btn = document.querySelector(".project-btn");
+
+let unlock = true;
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute("href").replace("#", "");
+      const curentPopup = document.getElementById(popupName); //получаем id попап-окна
+      popupOpen(curentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll(".popup-close");
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener("click", function (e) {
+      popupClose(el.closest(".popup")); //ближайший родитель класса popup
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open");
+    if (popupActive) {
+      // закрываем текущий открытый попап, если он есть
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    console.log(curentPopup);
+    curentPopup.classList.add("open");
+    curentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest(".popup__content")) {
+        // если клик был по области вокруг попапа то ничего не делаем
+        popupClose(e.target.closest(".popup"));
+      }
+    });
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove("open");
+    if (doUnlock) {
+      bodyUnLock();
+    }
+  }
+}
+
+// добавляем боди padding-right при открытии попапа, на ширину скролл-бара
+function bodyLock() {
+  const lockPaddingValue =
+    window.innerWidth - document.querySelector(".header").offsetWidth + "px";
+  console.log(lockPaddingValue);
+  for (let index = 0; index < lockPadding.length; index++) {
+    const el = lockPadding[index];
+    el.style.marginRight = lockPaddingValue;
+  }
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add("lock");
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index];
+      el.style.marginRight = "0px";
+    }
+    body.style.paddingRight = "0px";
+    body.classList.remove("lock");
+  }, timeout);
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    const popupActive = document.querySelector(".popup.open");
+    popupClose(popupActive);
+  }
 });
